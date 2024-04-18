18816b4c4454b04563222f0a50f312b3353a0286
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-04-18 16:17:24 +0200

    add db.php

--- index.php
+++ index.php
@@ -2,22 +2,46 @@
 
 require_once  './models/Production.php';
 
-$matrix = new Production('The Matrix', 'English', '9/10');
+require './models/db.php';
 
-$matrix->isGood($matrix->score);
+?>
 
-// $matrix->title = 'The Matrix';
+<!DOCTYPE html>
+<html lang="en">
 
-// $matrix->language = 'English';
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        body {
+            background-color: antiquewhite;
+        }
+    </style>
+</head>
 
-// $matrix->score = '8/10';
+<body>
 
-$cast_away = new Production('Cast Away', 'English', '8/10');
+    <div class="container">
+        <div class="row justify-content-center align-items-center g-2">
+            <div class="col-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title"><?php echo $matrix->title ?></h4>
+                        <p class="card-text"><?php echo $matrix->language ?></p>
+                        <p class="card-text"><?php echo $matrix->score ?></p>
+                    </div>
+                </div>
 
-var_dump($matrix);
+            </div>
+        </div>
 
-var_dump($cast_away);
+    </div>
 
-// $the_martian = new Production();
 
-// $it = new Production();
+</body>
+
+<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
+</html>
---
+++ models/db.php
@@ -0,0 +1,23 @@
+<?php
+
+$matrix = new Production('The Matrix', 'English', '9/10');
+
+$cast_away = new Production('Cast Away', 'English', '8/10');
+
+// $matrix->isGood($matrix->score);
+
+// $matrix->title = 'The Matrix';
+
+// $matrix->language = 'English';
+
+// $matrix->score = '8/10';
+
+
+
+// var_dump($matrix);
+
+// var_dump($cast_away);
+
+// $the_martian = new Production();
+
+// $it = new Production();
