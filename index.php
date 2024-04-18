8bbedebfc5e84ab2ec5e89a8b4d5ccc9c5e7c948
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-04-18 15:41:22 +0200

    add readme create class production and istanze

--- index.php
+++ index.php
@@ -2,4 +2,19 @@
 
 class Production
 {
+    public $title;
+    public $language;
+    public $score;
 }
+
+$matrix = new Production();
+
+$matrix->title = 'The Matrix';
+
+var_dump($matrix);
+
+$cast_away = new Production();
+
+$the_martian = new Production();
+
+$it = new Production();
