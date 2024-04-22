ab1d28912361563ee957c0ea6d56aacf4c174e37
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-04-22 09:51:30 +0200

    edit db

--- models/movies.php
+++ models/movies.php
@@ -5,7 +5,8 @@ require_once __DIR__ . '/Production.php';
 class Movies extends Production
 {
 
-    function __construct(public int $profits, public int $length)
+    function __construct(public string $title, public string $language, public int $score, public Genre $genre, public int $profits, public int $length)
     {
+        parent::__construct($title, $language, $score, $genre);
     }
 }
